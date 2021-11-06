6ee17c3258e78c9da4ea5eff922adfcfa6e209b4
MzAllison <isituaho@gmail.com>
2021-11-06 13:20:17 +0100

    header icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
+import Home from "./Home";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello REACT </h1>
+    <div className="app">
+     <BrowserRouter>
+        <Routes>
+            <Route path= "/search">
+                {/* <h1>This is the Search Page </h1> */}
+              
+              </Route>
+
+              <Route path="/" element={<Home />} />
+        </Routes>
+     </BrowserRouter>
+  
+
     </div>
   );
 }
